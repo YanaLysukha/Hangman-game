20ad1d9bb89921ae140d90e8fbf021a2651f8a5a
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-12 13:01:59 +0100

    fix: fix linter errors

--- hangman/js/index.js
+++ hangman/js/index.js
@@ -1,26 +1,26 @@
-function createNewElement (elementName, className) {
+function createNewElement(elementName, className) {
   const element = document.createElement(elementName);
   element.className = className;
   return element;
 }
 
-document.body.className = "body";
-const gameWrapper = createNewElement("div", "game-wrapper");
+document.body.className = 'body';
+const gameWrapper = createNewElement('div', 'game-wrapper');
 
 document.body.append(gameWrapper);
 
 // create gallows-side section
-const gallowsSideSection = createNewElement("section", "gallows-side");
+const gallowsSideSection = createNewElement('section', 'gallows-side');
 
 gameWrapper.append(gallowsSideSection);
 
-const gallowsSideImages = createNewElement("div", "gallows-side__images");
-const gallowsSideImg = createNewElement("img", "gallows-side__img");
-gallowsSideImg.src = "img/gallows.svg";
-gallowsSideImg.alt = "Gallows image";
+const gallowsSideImages = createNewElement('div', 'gallows-side__images');
+const gallowsSideImg = createNewElement('img', 'gallows-side__img');
+gallowsSideImg.src = 'img/gallows.svg';
+gallowsSideImg.alt = 'Gallows image';
 
-const gallowsSideTitle = createNewElement("h2", "gallows-side__title");
-gallowsSideTitle.innerText = "Hangman game";
+const gallowsSideTitle = createNewElement('h2', 'gallows-side__title');
+gallowsSideTitle.innerText = 'Hangman game';
 
 gallowsSideSection.append(gallowsSideImages);
 gallowsSideSection.append(gallowsSideTitle);
@@ -28,21 +28,21 @@ gallowsSideSection.append(gallowsSideTitle);
 gallowsSideImages.append(gallowsSideImg);
 
 // Create quiz-side section
-const quizSideSection = createNewElement("section", "quiz-side");
+const quizSideSection = createNewElement('section', 'quiz-side');
 
 gameWrapper.append(quizSideSection);
 
-const quizSideWord = createNewElement("div", "quiz-side__word");
+const quizSideWord = createNewElement('div', 'quiz-side__word');
 
 quizSideSection.append(quizSideWord);
 
-const spaceForLetter1 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter2 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter3 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter4 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter5 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter6 = createNewElement("div", "quiz-side__space-for-letter");
-const spaceForLetter7 = createNewElement("div", "quiz-side__space-for-letter");
+const spaceForLetter1 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter2 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter3 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter4 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter5 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter6 = createNewElement('div', 'quiz-side__space-for-letter');
+const spaceForLetter7 = createNewElement('div', 'quiz-side__space-for-letter');
 
 quizSideWord.append(spaceForLetter1);
 quizSideWord.append(spaceForLetter2);
@@ -52,46 +52,79 @@ quizSideWord.append(spaceForLetter5);
 quizSideWord.append(spaceForLetter6);
 quizSideWord.append(spaceForLetter7);
 
-const hintWrapper = createNewElement("div", "quiz-side__hint-wrapper");
+const hintWrapper = createNewElement('div', 'quiz-side__hint-wrapper');
 
 quizSideSection.append(hintWrapper);
 
-const hint = createNewElement("p", "quiz-side__hint");
-hint.innerText = "Hint:";
-const hintDescription = createNewElement("p", "quiz-side__hint-description");
-hintDescription.innerText = "A human-powered vehicle with two wheels.";
+const hint = createNewElement('p', 'quiz-side__hint');
+hint.innerText = 'Hint:';
+const hintDescription = createNewElement('p', 'quiz-side__hint-description');
+hintDescription.innerText = 'A human-powered vehicle with two wheels.';
 
 hintWrapper.append(hint);
 hintWrapper.append(hintDescription);
 
-const counterWrapper = createNewElement("div", "quiz-side__counter-wrapper");
+const counterWrapper = createNewElement('div', 'quiz-side__counter-wrapper');
 
 quizSideSection.append(counterWrapper);
 
-const counterText = createNewElement("p", "quiz-side__counter-text");
-counterText.innerText = "Incorrect guesses:";
-const counterNumbers = createNewElement("p", "quiz-side__counter-numbers");
-const changedCounter = createNewElement("span", "quiz-side__counter-numbers_changed");
-changedCounter.innerText = "0";
-const maxCounter = createNewElement("span", "quiz-side__counter-numbers-max");
-maxCounter.innerText = " / 6";
+const counterText = createNewElement('p', 'quiz-side__counter-text');
+counterText.innerText = 'Incorrect guesses:';
+const counterNumbers = createNewElement('p', 'quiz-side__counter-numbers');
+const changedCounter = createNewElement(
+  'span',
+  'quiz-side__counter-numbers_changed',
+);
+changedCounter.innerText = '0';
+const maxCounter = createNewElement('span', 'quiz-side__counter-numbers-max');
+maxCounter.innerText = ' / 6';
 
 counterWrapper.append(counterText);
 counterWrapper.append(counterNumbers);
 counterNumbers.append(changedCounter);
 counterNumbers.append(maxCounter);
 
-const keyboard = createNewElement("div", "quiz-side__keyboard");
+const keyboard = createNewElement('div', 'quiz-side__keyboard');
 
 quizSideSection.append(keyboard);
 
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const letters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
 function createNewKeyboardElement(arr) {
   return arr.forEach((el) => {
-    const keyboardLetter = createNewElement("div", "quiz-side__keyboard-letter");
+    const keyboardLetter = createNewElement(
+      'div',
+      'quiz-side__keyboard-letter',
+    );
     keyboardLetter.innerText = el;
     keyboard.append(keyboardLetter);
-  })
+  });
 }
 createNewKeyboardElement(letters);
