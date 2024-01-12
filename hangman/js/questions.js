48d6f17028835f73a15f490c455fd1adec1e5b0c
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-12 21:04:30 +0100

    feat: create questions object

---
+++ hangman/js/questions.js
@@ -0,0 +1,100 @@
+const questions = [
+  {
+    id: 1,
+    question: "A human-powered vehicle with two wheels.",
+    answer: "bicycle",
+  },
+
+  {
+    id: 2,
+    question: "What is a common pet with whiskers and a tail?",
+    answer: "cat",
+  },
+
+  {
+    id: 3,
+    question: "This country is known for its kangaroos and koalas.",
+    answer: "Australia",
+  },
+
+  {
+    id: 4,
+    question:
+      "It's a tropical fruit with a tough outer layer and sweet inner flesh.",
+    answer: "pineapple",
+  },
+
+  {
+    id: 5,
+    question: "The smallest unit of an element.",
+    answer: "atom",
+  },
+
+  {
+    id: 6,
+    question: "An ancient wonder in Egypt with a triangular shape.",
+    answer: "pyramids",
+  },
+
+  {
+    id: 7,
+    question:
+      "This Italian dish consists of thin layers of pasta, sauce, and cheese.",
+    answer: "lasagna",
+  },
+
+  {
+    id: 8,
+    question: "A popular mobile operating system developed by Google.",
+    answer: "Android",
+  },
+
+  {
+    id: 9,
+    question:
+      "This instrument is known for its black and white keys and is commonly found in classical music.",
+    answer: "Piano",
+  },
+
+  {
+    id: 10,
+    question:
+      "This iconic British rock band was formed in 1960 and is known for hits like 'Bohemian Rhapsody'.",
+    answer: "Queen",
+  },
+
+  {
+    id: 11,
+    question: "The fourth planet from the sun in our solar system.",
+    answer: "Mars",
+  },
+
+  {
+    id: 12,
+    question:
+      "A legendary creature with a single, spiraled horn on its forehead.",
+    answer: "Unicorn",
+  },
+
+  {
+    id: 13,
+    question:
+      "A Southeast Asian country known for its beautiful beaches and temples.",
+    answer: "Thailand",
+  },
+
+  {
+    id: 14,
+    question: "The process by which plants make their own food using sunlight.",
+    answer: "Photosynthesis",
+  },
+
+  {
+    id: 15,
+    question:
+      "A sport where players hit a small, lightweight ball back and forth over a net with a racket.",
+    answer: "Tennis",
+  },
+];
+
+export default questions;
