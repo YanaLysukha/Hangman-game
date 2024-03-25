83f07c38a665c6ef766a6b942726863036aae002
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-03-25 14:43:16 +0100

    chore: initial setup

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint-config-airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["prettier", "import", "@typescript-eslint"],
+  rules: {
+	indent: ['error', 4],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+	"no-console": 0,
+	"prettier/prettier": "error",
+  },
+};
---
+++ async-race/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const EslintPlugin = require("eslint-webpack-plugin");
+
+const baseConfig = {
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+      favicon: path.join(__dirname, "src", "favicon.ico"),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/img"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+      ],
+    }),
+    new EslintPlugin({ extensions: ["ts"] }),
+  ],
+  resolve: {
+    alias: {
+      img: path.join(__dirname, "src", "components", "view", "img"),
+    },
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
