e9a821fda014e0c3b03c8b0b0b1a5c1ce53e0a0e
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-06-18 13:27:28 +0200

    feat: create base component

---
+++ hangman/src/components/base-component/index.js
@@ -0,0 +1,25 @@
+export default class BaseComponent {
+  constructor(props, ...children) {
+    this.node = document.createElement(props.tag);
+
+    if (props.class) {
+      if (typeof props.class === "string") {
+        this.node.classList.add(props.class);
+      } else if (Array.isArray(props.class)) {
+        this.node.classList.add(...props.class);
+      }
+    }
+
+    if (props.text) {
+      this.node.textContent = props.text;
+    }
+
+    if (children) {
+      children.forEach((element) => {
+        this.node.append(element.node);
+      });
+    }
+
+    Object.assign(this.node, props);
+  }
+}
