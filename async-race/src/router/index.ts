156de2f421091f996697bb2d1ca5765c87f1c684
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-08-27 20:10:38 +0200

    feat: add router

---
+++ async-race/src/router/index.ts
@@ -0,0 +1,65 @@
+import { PageRouteKey, PageRoute, ROUTES } from "./routes";
+
+export default class Router {
+    private currentRoutePath: PageRouteKey;
+
+    private static instance: Router | null;
+
+    #list: PageRoute;
+
+    private constructor() {
+        this.currentRoutePath = window.location.pathname.slice(
+            1,
+        ) as PageRouteKey;
+        this.#list = ROUTES;
+        this.addPopStateEventListener();
+        window.history.replaceState(
+            this.currentRoutePath,
+            "",
+            document.location.href,
+        );
+    }
+
+    static getInstance = () => {
+        if (!Router.instance) {
+            Router.instance = new Router();
+        }
+        return Router.instance;
+    };
+
+    static isRouteExist = (route: string) => !!ROUTES[route as PageRouteKey];
+
+    addPopStateEventListener = () => {
+        window.addEventListener("popstate", (event) => {
+            if (event.state) {
+                this.route(
+                    window.location.pathname.slice(1) as PageRouteKey,
+                    true,
+                );
+            }
+        });
+    };
+
+    route = (routePath = this.currentRoutePath, needChangeHistory = true) => {
+        this.currentRoutePath = this.list().some(
+            (val) => val.routePath === routePath,
+        )
+            ? routePath
+            : "garage";
+
+        if (needChangeHistory) {
+            window.history.pushState(
+                this.currentRoutePath,
+                "",
+                `${this.currentRoutePath}`,
+            );
+        }
+    };
+
+    list = () =>
+        Object.entries(this.#list).map(([routePath, route]) => ({
+            routePath,
+            name: route.name,
+            routeToPage: () => this.route(routePath as PageRouteKey),
+        }));
+}
---
+++ async-race/src/router/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+    garage: {
+        name: "Garage",
+    },
+    winners: {
+        name: "Winners",
+    },
+};
+
+export type PageRoute = typeof ROUTES;
+export type PageRouteKey = keyof PageRoute;
