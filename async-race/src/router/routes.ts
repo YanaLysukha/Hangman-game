0d3dbeaafd4ce08fcad32c95bb84c546bf8e8b22
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-08-29 18:52:56 +0200

    feat: create new routes

--- async-race/src/router/routes.ts
+++ async-race/src/router/routes.ts
@@ -1,11 +1,32 @@
-export const ROUTES = {
-  garage: {
+import GaragePage from '@Src/pages/garage-page';
+import WinnersPage from '@Src/pages/winners-page';
+
+export enum AppRoutes {
+  GARAGE = '/garage',
+  WINNERS = '/winners',
+}
+
+type AvailablePage = GaragePage | WinnersPage;
+
+type Route = {
+  name: string;
+  page?: (args?: string[]) => AvailablePage | (() => AvailablePage);
+};
+
+type Routes = {
+  [key in AppRoutes]: Route;
+};
+
+const ROUTES: Routes = {
+  [AppRoutes.GARAGE]: {
     name: 'Garage',
+    page: () => new GaragePage(),
   },
-  winners: {
+  [AppRoutes.WINNERS]: {
     name: 'Winners',
+    page: () => new WinnersPage(),
   },
 };
 
 export type PageRoute = typeof ROUTES;
-export type PageRouteKey = keyof PageRoute;
+export default ROUTES;
