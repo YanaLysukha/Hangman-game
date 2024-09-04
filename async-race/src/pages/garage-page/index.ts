223bbe834f3ea0fee2be3205e64020aceb2d1fa1
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-09-04 12:23:34 +0200

    feat: get cars from server

--- async-race/src/pages/garage-page/index.ts
+++ async-race/src/pages/garage-page/index.ts
@@ -1,17 +1,35 @@
+import Api from '@Src/api';
+import BaseComponent from '@Src/components/common/base-component';
 import ContentPage from '@Src/components/common/content-page';
 import CarControlPanel from '@Src/components/ui/car-control-panel';
 import VehicleRaceControlPanel from '@Src/components/ui/vehicle-race-control-panel';
+import { ICar } from '@Src/types/interfaces';
+import classes from './style.module.scss';
 
 export default class GaragePage extends ContentPage {
+  carsInGarage!: ICar[];
+
+  carsContainer!: BaseComponent<HTMLDivElement>;
+
   constructor() {
     super({ containerTag: 'main', title: 'Garage' });
     this.createContent();
   }
 
   private createContent = () => {
-    this.container.node.append(
-      new CarControlPanel().node,
-      new VehicleRaceControlPanel('BMW', '#ff5733').node,
-    );
+    this.carsContainer = new BaseComponent<HTMLDivElement>({
+      tag: 'div',
+      class: classes.carsContainer,
+    });
+    this.container.node.append(new CarControlPanel().node, this.carsContainer.node);
+    this.createCars();
+  };
+
+  private createCars = async () => {
+    this.carsInGarage = await Api.getCars();
+    this.carsInGarage.forEach((carData) => {
+      const newCar = new VehicleRaceControlPanel(carData);
+      this.carsContainer.node.append(newCar.node);
+    });
   };
 }
